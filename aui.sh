d2b239285416339758661a3a0de4ed6c1a8db263
nlc <natechav1@gmail.com>
2022-05-19 17:06:29 -0500

    comment

--- aui.sh
+++ aui.sh
@@ -6,7 +6,7 @@ elif [[ ! $(type yq 2>/dev/null) ]]; then
   echo -e "Required utility \033[1myq\033[0m was not found. You can find instructions for installing it at \033[4mhttps://mikefarah.github.io/yq/\033[0m."
 else
   dict_key="$(yq eval 'keys' aui.yaml | sed 's/^- //' | fzf)"
-  echo "\$(yq eval ".${dict_key}" aui.yaml)"
+  # echo "\$(yq eval ".${dict_key}" aui.yaml)"
 
   dict_def="$(yq eval ".\"${dict_key}\"" aui.yaml)"
 
